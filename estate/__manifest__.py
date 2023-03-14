42b5f70bac7cb266dfec3d839d25d5c32d74a98d
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2023-03-14 11:41:41 -0600

    [ADD] estate: basic directory structure T#69469

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Real Estate',
+    'version': '16.0.1.0.0',
+    'author': 'Vauxoo',
+    'license': 'GPL-3 or any later version',
+    'depends': ['base'],
+    'application': True,
+    'installable': True,
+    'maintainer': 'Suavesito-Olimpiada',
+}
