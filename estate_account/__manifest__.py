267e9c106857bbfcebd99af82a7329c5bdbe712b
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2023-05-30 11:27:08 -0600

    [IMP] estate: add reports for properties and users T#69469
    In this commit reports are added, following the specifications of the
    section "Build PDF Reports" of the technical specification.
    
    Reports for individual properties were added, where it shows its offers.
    Reports for users where added too, allowing to see all the properties
    that they can manage and its corresponding offers.
    
    Additional demo data was added to test the creation of reports locally.

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -6,6 +6,7 @@
     'version': '16.0.1.0.0',
     'license': 'OPL-1',
     'depends': ['base', 'estate', 'account'],
+    'data': ['report/estate_property_templates.xml'],
     'auto_install': True,
     'application': True,
     'installable': True,
